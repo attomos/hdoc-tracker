e9761054e7e5c34f6ce8cf56dba3dc45272e3a4f
Nattaphoom Chaipreecha <attomos@gmail.com>
2022-02-06 00:31:29 +0700

    feat: construct date objects using utility from date-fns

--- src/lib/ActivityGraph.svelte
+++ src/lib/ActivityGraph.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
-  import { getDaysUntilNow as getWeeksUntilNow } from "./dateUtils";
+  import { onMount } from "svelte";
+
+  import { getWeeksUntilNow } from "./dateUtils";
 
   const array = Array(150).fill(1);
 
@@ -14,44 +16,81 @@
   const RECT_H_2 = RECT_H + 5;
   const START_X = 36;
   const START_Y = 5;
+
+  function mouseOverHandler(e: MouseEvent) {
+    const date = (e.target as SVGRectElement).getAttribute("data-date");
+    const tooltip = document.getElementById("tooltip");
+    tooltip.innerText = date;
+    tooltip.style.display = "block";
+    tooltip.style.left = e.pageX - 30 + "px";
+    tooltip.style.top = e.pageY - 40 + "px";
+
+    console.log(date);
+  }
+
+  function mouseLeaveHandler() {
+    const tooltip = document.getElementById("tooltip");
+    tooltip.innerText = "";
+    tooltip.style.display = "none";
+  }
+
+  onMount(async () => {
+    const rects = document.querySelectorAll("[data-date]");
+    rects.forEach((rect) => {
+      // rect.addEventListener("mouseover", mouseOverHandler);
+      rect.addEventListener("mousemove", mouseOverHandler);
+      rect.addEventListener("mouseleave", mouseLeaveHandler);
+    });
+  });
 </script>
 
-<svg
-  viewBox="0 0 900 140"
-  width="900"
-  height="140"
-  xmlns="http://www.w3.org/2000/svg"
-  class="border-2 border-black rounded-md"
->
-  {#each weeks as week, i}
-    <g
-      class="hover2:stroke-fuchsia-300 hover2:stroke-2"
-      transform="translate({i * RECT_W_2}, {START_Y})"
-    >
-      <rect
-        class="fill-[#ccfbf1]"
-        width={RECT_W + 4}
-        height={RECT_H * 12}
-        x={START_X - 2}
-        y={RECT_H_2 - 10}
-        rx="2"
-      />
-      {#each week as d, j}
+<!-- {@debug weeks} -->
+
+<div>
+  <div
+    id="tooltip"
+    class="text-xs bg-slate-700 rounded-md text-white py-2 px-1 absolute hidden"
+  >
+    mock data
+  </div>
+  <svg
+    viewBox="0 0 862 140"
+    width="862"
+    height="140"
+    xmlns="http://www.w3.org/2000/svg"
+    class="border-2 border-black rounded-md"
+  >
+    {#each weeks as week, i}
+      <g
+        class="hover2:stroke-fuchsia-300 hover2:stroke-2"
+        transform="translate({i * RECT_W_2}, {START_Y})"
+      >
         <rect
-          class="hover:cursor-pointer fill-gray-200 hover:fill-fuchsia-300 stroke-gray-400"
-          width={RECT_W}
-          height={RECT_H}
-          x={START_X}
-          y={j * RECT_H_2 + RECT_H_2}
+          class="fill-[#ccfbf1]"
+          width={RECT_W + 4}
+          height={RECT_H * 12}
+          x={START_X - 2}
+          y={RECT_H_2 - 10}
           rx="2"
         />
-      {/each}
-    </g>
-  {/each}
-  {#each DAYS as day, i}
-    <text class="text-xs" dx="0" dy={30 + 29 * i}>{day}</text>
-  {/each}
-</svg>
+        {#each week as d, j}
+          <rect
+            class="hover:cursor-pointer fill-gray-200 hover:fill-fuchsia-300 stroke-gray-400"
+            width={RECT_W}
+            height={RECT_H}
+            x={START_X}
+            y={j * RECT_H_2 + RECT_H_2}
+            rx="2"
+            data-date={d}
+          />
+        {/each}
+      </g>
+    {/each}
+    {#each DAYS as day, i}
+      <text class="text-xs" dx="0" dy={30 + 29 * i}>{day}</text>
+    {/each}
+  </svg>
+</div>
 
 <style>
 </style>
--- src/lib/dateUtils.ts
+++ src/lib/dateUtils.ts
@@ -1,3 +1,11 @@
+import {
+  startOfYear,
+  startOfWeekWithOptions,
+  format,
+  endOfWeekWithOptions,
+  endOfYear,
+} from "date-fns/fp";
+
 function getAddedDate(original: Date, days: number) {
   var date = new Date(original.valueOf());
   date.setDate(date.getDate() + days);
@@ -14,13 +22,22 @@ function getDateRange(startDate: Date, endDate: Date) {
   return dates;
 }
 
-export function getDaysUntilNow() {
-  const range = getDateRange(new Date("2022-01-01"), new Date());
+export function getWeeksUntilNow() {
+  const today = new Date();
+  const firstMondayOfCalendar = startOfWeekWithOptions({
+    weekStartsOn: 1,
+  })(startOfYear(today));
+
+  const lastSundayOfCalendar = endOfWeekWithOptions({
+    weekStartsOn: 1,
+  })(endOfYear(today));
+
+  const range = getDateRange(firstMondayOfCalendar, lastSundayOfCalendar);
   const weeks = [];
   let aWeek = [];
 
   for (let day of range) {
-    aWeek.push(day);
+    aWeek.push(format("yyyy-MM-dd")(day));
     if (aWeek.length === 7) {
       weeks.push(aWeek);
       aWeek = [];
