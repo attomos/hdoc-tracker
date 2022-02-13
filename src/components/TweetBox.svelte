c22308554376f0f4c3bef8c3eae98cadcc61dfef
Nattaphoom Chaipreecha <attomos@gmail.com>
2022-02-13 09:59:41 +0700

    feat: focused state and keyboard navigation (wip)

--- src/components/TweetBox.svelte
+++ src/components/TweetBox.svelte
@@ -4,6 +4,9 @@
   export let tweet: Tweet;
   const tweetHtmlText = tweet.text.replaceAll("\n", "<br>");
 
+  // export let tweet;
+  // const tweetHtmlText = tweet.text.replaceAll("\n", "<br>") + tweet.idx;
+
   let active = false;
 
   function handleFocus() {
@@ -17,17 +20,76 @@
   function handleMouseOut() {
     active = false;
   }
+
+  function focusItem(element) {
+    element.setAttribute("aria-selected", "true");
+    element.classList.add("focused");
+    const rect = element.getBoundingClientRect();
+    console.log(rect);
+    console.log(element.offsetTop);
+    const { y, height } = element.getBoundingClientRect();
+    const ulParent = document.querySelector("#tweet-list").parentElement;
+    console.log(ulParent);
+    // ulParent.scrollBy(0, y - height - 80);
+    // ulParent.scrollBy(0, height);
+    // ulParent.scrollTo(element.offsetTop, 0);
+    // ul.scrollBy()
+  }
+
+  function findNextOption(currentOption) {
+    const ul = document.querySelector("#tweet-list");
+    const listItems = Array.from(ul.querySelectorAll("li"));
+    const currentIndex = listItems.indexOf(currentOption);
+    const nextItem =
+      currentIndex + 1 < listItems.length
+        ? listItems[currentIndex + 1]
+        : listItems[0];
+    return nextItem;
+  }
+
+  function findPrevOption(currentOption) {
+    const ul = document.querySelector("#tweet-list");
+    const listItems = Array.from(ul.querySelectorAll("li"));
+    const currentIndex = listItems.indexOf(currentOption);
+    const nextItem =
+      currentIndex > 0
+        ? listItems[currentIndex - 1]
+        : listItems[listItems.length - 1];
+    return nextItem;
+  }
+
+  function handleKeyDown(e) {
+    const currentItem =
+      document.querySelector("#tweet-list li[aria-selected='true']") ||
+      document.querySelector("#tweet-list li[aria-selected]:first-child");
+
+    if (e.key === "ArrowDown" || e.key === "j") {
+      const nextItem = findNextOption(currentItem);
+      currentItem.setAttribute("aria-selected", "false");
+      currentItem.classList.remove("focused");
+      focusItem(nextItem);
+    } else if (e.key === "ArrowUp" || e.key === "k") {
+      const prevItem = findPrevOption(currentItem);
+      currentItem.setAttribute("aria-selected", "false");
+      currentItem.classList.remove("focused");
+      focusItem(prevItem);
+    }
+    e.preventDefault();
+  }
 </script>
 
 <!-- {@debug tweet} -->
+<!-- on:mouseout={handleMouseOut} -->
+<!-- on:focus={handleFocus}
+on:mouseover={handleMouseOver}
+on:blur={handleMouseOut}
+ -->
 
 <li
   role="option"
   aria-selected={active}
-  on:focus={handleFocus}
-  on:mouseover={handleMouseOver}
-  on:mouseout={handleMouseOut}
-  on:blur={handleMouseOut}
+  on:keydown={handleKeyDown}
+  class={active ? "" : ""}
 >
   {#if tweet.conversation_id === tweet.id}
     <a class="tweet-box" href="#content">
--- src/components/Tweets.svelte
+++ src/components/Tweets.svelte
@@ -6,7 +6,12 @@
 <div class="mt-4 flex flex-col flex-grow overflow-scroll">
   <h2 class="text-lg font-bold my-4">List of tweets go here...</h2>
   <div class="flex-grow overflow-scroll pb-40">
-    <ul class="ml-8" role="listbox">
+    <ul class="mx-8 py-2" role="listbox" id="tweet-list">
+      <!-- {#each $tweets.data.slice(0, 10) as tweet, idx}
+        <TweetBox
+          tweet={{ text: "something short", conversation_id: 1, id: 1, idx }}
+        />
+      {/each} -->
       {#each $tweets.data as tweet}
         <TweetBox {tweet} />
       {/each}
