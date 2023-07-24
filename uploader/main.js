f5c340b95358877947579799408fed9681983a23
Nattaphoom Chaipreecha <attomos@gmail.com>
2023-07-24 15:47:06 +0700

    feat: change uploader to use JSON SET command instead

--- uploader/main.js
+++ uploader/main.js
@@ -7,8 +7,7 @@ dotenv.config();
 const redis = Redis.fromEnv();
 
 (async function run() {
-  // const data = fs.readFileSync("../mastodon.json", "utf-8");
   const data = fs.readFileSync("../scripts/grouped_1_converted.json", "utf-8");
-  const res = redis.set("round1_converted", data);
+  const res = redis.json.set("round1", "$", data);
   console.log(res);
 })();
